2fdbd7a4f40337723883c399c0117def3c9eb0af
Eric Wagner <github@alphamule.com>
2021-07-15 19:17:36 -0700

    create desktop version of Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+        <SubscribeGroup>
+          <Button>Subscribe</Button>
+          <a href="/#">Already a subscriber?</a>
+        </SubscribeGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,44 @@ const ActionGroup = styled.div`
   }
 `;
 
+const SubscribeGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+
+  a {
+    text-decoration: underline;
+    font-style: italic;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  ${SubscribeGroup} {
+    display: none;
+  }
+
+  ${ActionGroup} {
+    display: none;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+
+    ${SubscribeGroup} {
+      display: flex;
+    }
+
+    ${ActionGroup} {
+      display: flex;
+    }
+  }
 `;
 
 export default Header;
